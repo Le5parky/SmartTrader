ed81ab86cae011a8c1fb7635bf76e7d570bf6ec2
Le5parky <Le5parky@users.noreply.github.com>
2024-05-24 13:18:59 +0000

    Setup Autofac

--- SmartTrader.Main/Program.cs
+++ SmartTrader.Main/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.Collections.Immutable;
+using System.Threading;
+using Autofac;
+
+using var mainCts = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true; 
+    mainCts.Cancel();
+
+    Console.WriteLine("Shutting down gracefully...");
+    // Perform any necessary cleanup here before exiting the application.
+    // After cleanup, you might want to exit the application by Environment.Exit(0);
+};
+
+var builder = new ContainerBuilder();
+builder.RegisterType<Startup>().As<IService>();
+
+var container = builder.Build();
+
+
+using (var scope = container.BeginLifetimeScope())
+    {
+        var service = scope.Resolve<IService>();
+        service.Execute();
+    }
+
+public interface IService
+{
+    void Execute();
+}
+
+public class Startup:IService
+{
+    public void Execute()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+} 
